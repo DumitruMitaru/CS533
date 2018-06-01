2b7f8b96b1aacac8ef0647f1e5261f82592f6e09
Dumitru Mitaru-Berceanu <mitarub2@ada.cs.pdx.edu>
2018-06-01 15:49:04 -0700

    initial commit

---
+++ template.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[])
+{
+	struct timeval start;
+	struct timeval end;
+	long diff;
+	int rc;
+	// Extra Code
+	gettimeofday(&start, NULL);
+	rc = XXX;
+	gettimeofday(&end, NULL);
+	if(rc == -1)
+		printf("-1,");
+	else
+		diff = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+	printf("%li,", diff);
+}
