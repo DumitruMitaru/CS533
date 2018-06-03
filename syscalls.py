8c6193b1e2968cd3c696af0ca80c75457ca88a78
Jacob Crisan <jacob.crisan@gmail.com>
2018-06-02 21:01:39 -0700

    Added some more information management system calls that I thought might be used somewhat commonly or that I felt were noteworthy (namely, getuid and setuid)

--- syscalls.py
+++ syscalls.py
@@ -20,10 +20,18 @@ systemcalls = [
 # Device Management
 # Information Maintenence
 	('getrusage', 'syscall(SYS_getrusage, RUSAGE_SELF, &usage)', 'struct rusage usage;', 1000),
-	('gettimeofday', 'syscall(SYS_gettimeofday, &t, NULL);', 'struct timeval t;', 1000),
+        ('stat', 'syscall(SYS_stat, "test.txt", &stat_buffer)', 'struct stat stat_buffer;', 1000),
+        ('statfs', 'syscall(SYS_statfs, "test.txt", &statfs_buffer)', 'struct statfs statfs_buffer;', 1000),
+        ('time', 'syscall(SYS_time, &time_struct)', 'time_t time_struct;', 1000),
         ('clock_gettime', 'syscall(SYS_clock_gettime, _POSIX_CPUTIME, &time_spec)', 'struct timespec time_spec;', 1000), 
-        ('clock_getres', 'syscall(SYS_clock_getres, _POSIX_CPUTIME, &time_spec)', 'struct timespec time_spec;', 1000),
+#        ('clock_getres', 'syscall(SYS_clock_getres, _POSIX_CPUTIME, &time_spec)', 'struct timespec time_spec;', 1000),
 #        ('clock_settime', 'syscall(SYS_clock_settime, _POSIX_CPUTIME, &time_spec)', 'struct timespec time_spec; time_spec.tv_sec = 64; time_spec.tv_nsec = 64;', 1000),
+	('gettimeofday', 'syscall(SYS_gettimeofday, &time_val, NULL);', 'struct timeval time_val;', 1000),
+#        ('getpid', 'syscall(SYS_getpid)', '', 1000),
+#        ('getppid', 'syscall(SYS_getpid)', '', 1000),
+        ('getuid', 'syscall(SYS_getpid)', '', 1000),
+        ('setuid', 'syscall(SYS_setuid, val)', 'uid_t val = getuid();', 1000), # Setting the process uid to the already existing uid should take as long as setting it elsewhere. Also, permissions may prohibit using any other value easily
+ #       ('getgid', 'syscall(SYS_getppid)', '', 1000),
 # Communication
 # ('sigaction', 'syscall(SYS_sigaction,...);, '', 1000) 
 # ('sigreturn', 'syscall(SYS_sigreturn,...);, '', 1000) 
@@ -70,3 +78,11 @@ def average(output):
 CreateNewFiles()
 Run()
 DeleteFiles()
+
+
+# References Used
+# https://stackoverflow.com/questions/8798761/c-error-storage-size-of-a-isn-t-known
+# https://stackoverflow.com/questions/8812959/how-to-read-linux-file-permission-programmatically-in-c-c
+# http://man7.org/linux (various pages from here. Examples include http://man7.org/linux/man-pages/man2, and http://man7.org/linux/man-pages/man2/clock_getres.2.html)
+# http://seclab.cs.sunysb.edu/sekar/papers/syscallclassif.htm
+# https://linux.die.net/man/3/clock_settime 
