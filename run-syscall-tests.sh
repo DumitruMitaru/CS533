51a5f1152b1f612c6d2522d115575a364a1964fd
Jacob Crisan <jacob.crisan@gmail.com>
2018-06-12 21:40:59 -0700

    Forgot to include bash script that runs the python data gathering script multiple times in the previous commits. It is being added now.

--- run-syscall-tests.sh
+++ run-syscall-tests.sh
@@ -2,7 +2,7 @@
 # Project Group 1 (A2)
 # File Name: run-syscall-tests.sh
 # Purpose: to run the system call test script a set number of times (for data gathering purposes)
-# Script was inspired by data gathering script from previous course
+# Script was inspired by data gathering script from previous course (CS410: Intro to Performance)
 
 # Inspired by data gathering script written in a previous course
 # Required Looping Variables
@@ -10,7 +10,8 @@ COUNT=1
 MAX=31
 
 # Output file
-OUTFILE=syscall-test-output.txt
+# Allows user to specify the path of the output file by passing it in as a paremeter
+OUTFILE=$1
 
 while [ $COUNT -lt $MAX ]; do
 	# Run Test
